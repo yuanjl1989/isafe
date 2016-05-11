7d5a3ca703186a20d2254ec23a2d159a672b38dc
yuanjunlei <yuanjunlei@metersbonwe.com>
2016-05-11 18:02:02 +0800

    first commit

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+//    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=mb_safe',
+    'username' => 'root',
+    'password' => 'mbdev321',
+    'charset' => 'utf8',
+];
