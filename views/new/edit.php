15720578684c0f393ce2f7f90f7e033a184e1230
yuanjunlei <yuanjunlei@metersbonwe.com>
2016-05-31 17:09:22 +0800

    调整新增/编辑页面表单

--- views/new/edit.php
+++ views/new/edit.php
@@ -33,16 +33,16 @@ $this->params['breadcrumbs'][] = $this->title;
 
                 <?php $form = ActiveForm::begin(['id' => 'edit-form']); ?>
 
-                <?= $form->field($model, 'id')->hiddenInput()->label('') ?>
+                <?= $form->field($model, 'id')->hiddenInput()->label(false) ?>
                 <?= $form->field($model, 'url')->textInput(['placeholder'=>'请输入需要扫描的网站地址','required'=>'required','type'=>'url'])->label('网站地址') ?>
                 <?= $form->field($model, 'profile')->dropDownList(['1'=>'默认（均检测）','2'=>'AcuSensor传感器','3'=>'SQL盲注','4'=>'跨站点请求伪造','5'=>'目录和文件检查','6'=>'空（不使用任何检测）','7'=>'文件上传','8'=>'谷歌黑客数据库','9'=>'高风险警报','10'=>'网络脚本','11'=>'参数操纵','12'=>'SQL注入','13'=>'文本搜索','14'=>'弱口令','15'=>'Web应用程序','16'=>'跨站脚本攻击'])->label('扫描配置') ?>
                 <?= $form->field($model, 'mode')->dropDownList(['1'=>'快速','2'=>'混合','3'=>'广泛'])->label('扫描模式') ?>
-                <?= $form->field($model, 'is_mail')->radioList(['1'=>'开','2'=>'关'],['class'=>'radio-inline'])->label('邮件通知') ?>
+                <?= $form->field($model, 'is_mail')->inline()->radioList(['1'=>'是','2'=>'否'],['class'=>'radio-inline'])->label('邮件通知') ?>
 
                 <div>
                     <hr/>
                     <?= $form->field($model, 'login_username')->textInput(['placeholder'=>'账号'])->label('表单认证') ?>
-                    <?= $form->field($model, 'login_password')->passwordInput(['placeholder'=>'密码'])->label('') ?>
+                    <?= $form->field($model, 'login_password')->passwordInput(['placeholder'=>'密码'])->label(false) ?>
                     <p>提示：若系统存在登录认证模块，请填写账号、密码</p>
                     <br/>
                 </div>
--- views/new/new.php
+++ views/new/new.php
@@ -29,12 +29,12 @@ $this->params['breadcrumbs'][] = $this->title;
                 <?= $form->field($model, 'url')->textInput(['placeholder'=>'请输入需要扫描的网站地址'])->label('网站地址') ?>
                 <?= $form->field($model, 'profile')->dropDownList(['1'=>'默认（均检测）','2'=>'AcuSensor传感器','3'=>'SQL盲注','4'=>'跨站点请求伪造','5'=>'目录和文件检查','6'=>'空（不使用任何检测）','7'=>'文件上传','8'=>'谷歌黑客数据库','9'=>'高风险警报','10'=>'网络脚本','11'=>'参数操纵','12'=>'SQL注入','13'=>'文本搜索','14'=>'弱口令','15'=>'Web应用程序','16'=>'跨站脚本攻击'])->label('扫描配置') ?>
                 <?= $form->field($model, 'mode')->dropDownList(['1'=>'快速','2'=>'混合','3'=>'广泛'])->label('扫描模式') ?>
-                <?= $form->field($model, 'is_mail')->radioList(['1'=>'开','2'=>'关'],['class'=>'radio-inline'])->label('邮件通知') ?>
+                <?= $form->field($model, 'is_mail')->inline()->radioList(['1'=>'是','2'=>'否'])->label('邮件通知') ?>
 
                 <div>
                     <hr/>
                     <?= $form->field($model, 'login_username')->textInput(['placeholder'=>'账号'])->label('表单认证') ?>
-                    <?= $form->field($model, 'login_password')->passwordInput(['placeholder'=>'密码'])->label('') ?>
+                    <?= $form->field($model, 'login_password')->passwordInput(['placeholder'=>'密码'])->label(false) ?>
                     <p>提示：若系统存在登录认证模块，请填写账号、密码</p>
                     <br/>
                 </div>
