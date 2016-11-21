0ea39a9d65e645b0f2b00570b9113a0442538c01
yuanjunlei <yuanjunlei@metersbonwe.com>
2016-11-21 11:00:32 +0800

    取消成功后，刷新页面

--- web/js/list.js
+++ web/js/list.js
@@ -1,30 +1,16 @@
 function docancel(safe_id) {
 
     if(confirm('你确定要取消该申请么？')){
-        window.location.reload();
         $.ajax({
             type: "POST",
             dataType: "json",
             data: {},
             url: "/site/cancel?id="+safe_id,
             success: function () {
+                window.location.reload();
             }
         });
     }
 }
-
-$('#new_safe').on('click', function () {
-    layer.confirm('', {
-        title: false,
-        content: '请选择扫描工具并前往',
-        closeBtn: 0,
-        shadeClose: 1,
-        btn: ['WVS -> Go','Appscan -> Go'] //按钮
-    }, function () {
-        self.location='/new/new';
-    }, function () {
-        self.location='/new/new?tool=appscan';
-    });
-});
 
 
