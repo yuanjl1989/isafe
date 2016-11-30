14b52b14de49cb8040a343afb4213e8ce3b2831c
yuanjunlei <yuanjunlei@metersbonwe.com>
2016-11-30 15:49:43 +0800

    调整登录机制，使用ldap及本地数据库

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -57,13 +57,21 @@ class LoginForm extends Model
     public function login()
     {
         $user_info = $this->getUser();
-        if ($this->validate()) {
-            Yii::$app->session['staff_no'] = $user_info['staff_no'];
-            Yii::$app->session['user_id'] = $user_info['id'];
-            Yii::$app->session['email'] = $user_info['email'];
-            if(Yii::$app->session['url']){
-                Header("Location: ".Yii::$app->session['url']);
-            }else{
+        if ($user_info) {
+            if ($this->validate() || $this->auth_login($this->username, $this->password)) {
+                Yii::$app->session['staff_no'] = $user_info['staff_no'];
+                Yii::$app->session['user_id'] = $user_info['id'];
+                Yii::$app->session['email'] = $user_info['email'];
+                if (Yii::$app->session['url']) {
+                    Header("Location: " . Yii::$app->session['url']);
+                } else {
+                    Header("Location: /");
+                }
+            }
+        } elseif ($this->insertUser()) {
+            if (Yii::$app->session['url']) {
+                Header("Location: " . Yii::$app->session['url']);
+            } else {
                 Header("Location: /");
             }
         }
@@ -78,9 +86,62 @@ class LoginForm extends Model
     public function getUser()
     {
         if ($this->_user === false) {
-            $this->_user = User::find()->where(['staff_no'=>$this->username])->one();
+            $this->_user = User::find()->where(['staff_no' => $this->username])->one();
+            echo $this->username;
         }
 
         return $this->_user;
+    }
+
+    private function auth_login($username, $password)
+    {
+        if (!empty($password) && !empty($password)) {
+
+            $domain = 'mb.com'; //设定域名
+            $basedn = 'dc=mb,dc=com'; //如果域名为“b.a.com”,则此处为“dc=b,dc=a,dc=com”
+
+            $ad = @ldap_connect("ldap://192.168.203.11:389/");
+            @ldap_set_option($ad, LDAP_OPT_PROTOCOL_VERSION, 3);
+            @ldap_set_option($ad, LDAP_OPT_REFERRALS, 0);
+            @ldap_bind($ad, "{$username}@{$domain}", $password);
+
+            $filter = 'name=' . $username;
+            $result = @ldap_search($ad, $basedn, $filter);
+            $info = @ldap_get_entries($ad, $result);
+
+            return !$result ? 0 : array(1, $info);
+        }
+    }
+
+    public function insertUser()
+    {
+        $inputarr = array();
+        $auth = $this->auth_login($this->username, $this->password);
+        if (is_array($auth)) {
+            $userinfo = $auth[1][0];
+            $inputarr['chinese_name'] = $userinfo['displayname'][0];
+            $inputarr['english_name'] = $userinfo['mailnickname'][0];
+            $inputarr['username'] = $userinfo['mail'][0];
+            $inputarr['staff_no'] = $userinfo['name'][0];
+
+            $user = new User();
+            $date = date('Y-m-d H:i:s');
+            $user->email = $inputarr['username'];
+            $user->password_hash = '';
+            $user->chinese_name = $inputarr['chinese_name'];
+            $user->english_name = $inputarr['english_name'];
+            $user->staff_no = $inputarr['staff_no'];
+            $user->created_at = $date;
+            $user->updated_at = $date;
+            $user->is_deleted = 0;
+            $user->save();
+
+            Yii::$app->session['staff_no'] = $inputarr['staff_no'];
+            Yii::$app->session['user_id'] = $user->attributes['id'];
+            Yii::$app->session['email'] = $inputarr['username'];
+
+            return $user->attributes['id'];
+        }
+        return false;
     }
 }
